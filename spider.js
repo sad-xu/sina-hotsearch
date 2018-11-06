87d57de94c08eab4fa21b16d1b2961a993e4ecb4
xhc <1031568754@qq.com>
2018-11-07 00:38:27 +0800

    add 'save data'

--- spider.js
+++ spider.js
@@ -4,7 +4,7 @@ const axios = require('axios')
 const log = require('./log.js').getLogger('err')
 
 const mongoose = require('mongoose')
-const Hotword = require('./models/hotword')
+const Hotword = require('./model/hotword')
 
 // 微博国际版api
 // const APP_URL = 'http://overseas.weico.cc/portal.php?ct=feed&a=search_topic'
@@ -15,12 +15,24 @@ const WEB_URL = 'https://m.weibo.cn/api/container/getIndex?containerid=106003typ
 mongoose.connect('mongodb://localhost/SINA', { useNewUrlParser: true })
 	.then(res => console.log('数据库连接成功'))
 	.catch(err => console.log(err))
-mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise
 
 // 存储
-// function saveData() {
-// 	Hotword.findOneAndUpdate()
-// }
+function saveData(d) {
+	// {desc: String, t: Number, n: Number}
+	Hotword.findOneAndUpdate(
+		{ desc: d.desc },
+		{ $push: { 
+				timeline: {t: d.t, n: d.n} 
+			}
+		},
+		{ upsert: true }
+	).then(res => {
+
+	}).catch(err => {
+		log.error(d, err)
+	})
+}
 
 
 // 获取数据
@@ -41,16 +53,16 @@ function getData() {
 // 移动端页面 处理数据
 function webProcessData(data) {
 	if (data.ok !== 1) return log.error('error:', data)
-	let arr = []
+	let time = Math.floor(new Date().getTime() / 1000)
 	data.data.cards[0].card_group.forEach(item => {
 		if (item.desc_extr) {
-			arr.push({
-				desc: item.desc,
-				num: Number(item.desc_extr)
+			saveData({
+				desc: item.desc, 
+				t: time, 
+				n: Number(item.desc_extr)
 			})
 		}
 	})
-	console.log(arr)
 }
 
 
@@ -58,7 +70,8 @@ function webProcessData(data) {
 
 console.log('start...')
 
+setTimeout(function() {
+	getData()
+}, 3000)
 
 
-
-getData()
