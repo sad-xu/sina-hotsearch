9b9fe93f821742bf061b4896ff92adafc999f438
xhc <1031568754@qq.com>
2019-04-24 02:11:26 +0800

    finish check permission && doing upload img

--- robot.js
+++ robot.js
@@ -2,23 +2,111 @@
  * 定时发微博
  *
  */
+const fs = require('fs')
+const request = require('request')
+const querystring = require('querystring')
 const Login = require('./robot/login.js').Login
 
-let cookie = ''
+let cookie = 'SCF=AldmGDEwDPkD92RXILczZb2G8pPApbPauTIKHBTujc57QXAslK-sLHrWCgsRG1Gl-AQPsxAbIPZRXniM9zY4Ydo.; SUB=_2A25xu1A6DeRhGeFO41AT9CbPyzmIHXVSscbyrDV8PUJbmtBeLRSkkW9NQV6UFUu0kxYJRe2FtiGpn9f3thO4HoSH; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9Whz9ANA37kvuy-dhk3ZZW6c5JpX5K2hUgL.FoM71hzEShn0eh-2dJLoI05LxKML1heLB-BLxKqL1K5L1-eLxK-L1hnL1hqLxKqL1--L1K5LxK-LBonL1hybdNBt; SUHB=0eb9sZ1rRPZAco; SRT=D.QqHBJZPtWOEBi!Mb4cYGS4SLiDEBPDbDRbBu5csHNEYd43iZRebpMERt4EP1RcsrAcPJ4bihTsVuObEdibPFPQ4nT3YSNbHbJ!SrT4WnUZbcJ3MfOqMzUsiz*B.vAflW-P9Rc0lR-ykeDvnJqiQVbiRVPBtS!r3JZPQVqbgVdWiMZ4siOzu4DbmKPWf5ZbBO!mqi-k-WQ!kSFYhJs9TPqSZi49ndDPIJcYPSrnlMcyiiqEf5!POTFtnSdXkJcM1OFyHJDPJ5mkiODEfS4oCI4HJ5mkoODEIi4noIdPJ5mjkODEfU!noTGEJ5mkoODmkI4noNqPJ5mjkOmH6U!noTGb8SmuCWv77; SRF=1556029546; SSOLoginState=1556029546'
 
+// 登陆 获取cookie
 function getCookie() {
-  return new Login('1031568754@qq.com', 'xhc151136').init()
+  return new Promise((resolve, reject) => {
+    new Login('1031568754@qq.com', 'xhc151136')
+      .init()
+      .then(res => {
+        cookie = res
+        resolve(res)
+      }).catch(err => {
+        reject(err)
+      })
+  })
 }
 
-getCookie().then(res => {
-  cookie = res
-  console.log(res)
-}).catch(err => {
-  console.log(err)
-})
+// 点赞固定微博，检验cookie是否有效
+function hasPermission() {
+  return new Promise((resolve, reject) => {
+    request({
+      url: 'https://weibo.com/aj/v6/like/add?ajwvr=6&__rnd=',
+      method: 'POST',
+      headers: {
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0",
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Connection': 'Keep-Alive',
+        "Referer": 'https://weibo.com/u/2671109275?profile_ftype=1&is_hot=1',
+        'cookie': cookie
+      },
+      gzip: true,
+      credentials: "include",
+      body: querystring.stringify({
+        location: 'page_100606_home',
+        version: 'mini',
+        qid: 'heart',
+        mid: 4363901967912825,
+        loc: 'profile',
+        cuslike: 1,
+        floating: 0,
+        _t: 0
+      })
+    }, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        response.setEncoding('utf-8');
+        let data = JSON.parse(response.body)
+        if (data.code === '100000') {
+          resolve(true)
+        } else reject(false)
+      } else reject(false)
+    })
+  })
+}
+
+// 上传图片
+function uploadPic(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'base64', (err, imgBase64Data) => {
+      if (err) console.log(err)
+      request({
+        url: `https://picupload.weibo.com/interface/pic_upload.php?mime=image%2Fjpeg&data=base64&url=0&markpos=1&logo=&nick=0&marks=0&app=miniblog&s=rdxt&pri=null&file_source=1&callback=STK_ijax_155603547178672`,
+        method: 'POST',
+        headers: {
+          "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0",
+          'Accept-Language': 'en-US,en;q=0.5',
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Connection': 'Keep-Alive',
+          "Referer": 'https://weibo.com/u/5330776018/home',
+          'Host': 'picupload.weibo.com',
+          'Origin': 'https://weibo.com',
+          'Upgrade-Insecure-Requests': 1,
+          'Cache-Control': 'max-age=0',
+          'Content-Length': 4259,
+          'cookie': cookie
+        },
+        gzip: true,
+        credentials: "include",
+        body: querystring.stringify({
+          b64_data: encodeURIComponent(imgBase64Data.toString())
+        })
+      }, (error, response, body) => {
+        if (!error) {
+          console.log(response.headers, body)
+          resolve('s')
+        } else reject('上传图片失败')
+      })
+    })
+  })
+}
 
 
-
+(async () => {
+  // const isLogin = await hasPermission()
+  // if (!isLogin) { // token失效，重新登录
+  //   console.log('重新登录')
+  //   await getCookie()
+  // }
+  // 发微博
+  await uploadPic('./robot/testpic2.png')
+})()
 
 
 
